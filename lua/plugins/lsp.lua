a11cf53828683c1f1f87cd603c41af5f41a02277
Sebastian Kuiter <sebastian.kuiter@krone.de>
2024-09-17 10:40:42 +0200

    nothing

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -16,6 +16,7 @@ return {
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require("cmp_nvim_lsp")
+
         local capabilities = vim.tbl_deep_extend(
             "force",
             {},
