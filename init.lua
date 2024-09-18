59502eda8623486d57720930efae46009a888a32
Sebastian Kuiter <sebastian.kuiter@krone.de>
2024-09-18 11:31:26 +0200

    added win32yank for clipboard connection

--- init.lua
+++ init.lua
@@ -2,3 +2,18 @@ require("kuiters")
 require("kuiters.lazy")
 
 vim.cmd.colorscheme("rose-pine-moon")
+
+if vim.fn.has('wsl') == 1 then
+  vim.g.clipboard = {
+    name = 'win32yank',
+    copy = {
+      ['+'] = 'win32yank.exe -i --crlf',
+      ['*'] = 'win32yank.exe -i --crlf',
+    },
+    paste = {
+      ['+'] = 'win32yank.exe -o --lf',
+      ['*'] = 'win32yank.exe -o --lf',
+    },
+    cache_enabled = 0,
+  }
+end
